174a5fe7c989391e2719277c9a78866d29c83060
Léo Beauvais Gély <leobgely@gmail.com>
2026-02-12 12:08:23 +0100

    add deceleration

--- Knight-Island/Assets/Scripts/Characters/PlayerMovement.cs
+++ Knight-Island/Assets/Scripts/Characters/PlayerMovement.cs
@@ -27,11 +27,42 @@ namespace Characters
         {
             Vector2 direction = _playerInputs.Movements.Direction.ReadValue<Vector2>();
             
-            rb.linearVelocity += direction * (acceleration * Time.fixedDeltaTime);
+            Accelerate(direction);
 
+            ClampSpeed();
+            
+            Decelerate(direction);
+        }
+
+        private void Accelerate(Vector2 direction)
+        {
+            rb.linearVelocity += direction * (acceleration * Time.fixedDeltaTime);
+        }
+
+        private void ClampSpeed()
+        {
             if (rb.linearVelocity.magnitude > maxSpeed)
             {
                 rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+        }
+
+        private void Decelerate(Vector2 direction)
+        {
+            Vector2 velocityDirection = rb.linearVelocity.normalized;
+
+            float inputDeltaX = Mathf.Abs(rb.linearVelocityX + direction.x);
+            float inputDeltaY = Mathf.Abs(rb.linearVelocityY + direction.y);
+
+            
+            if (velocityDirection.x != 0 && inputDeltaX <= Mathf.Abs(rb.linearVelocityX))
+            {
+                rb.linearVelocityX -= velocityDirection.x * deceleration * Time.fixedDeltaTime;
+            }
+
+            if (velocityDirection.y != 0 && inputDeltaY <= Mathf.Abs(rb.linearVelocityY))
+            {
+                rb.linearVelocityY -= velocityDirection.y * deceleration * Time.fixedDeltaTime;
             }
         }
     }
