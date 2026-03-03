87e55530ba91c1d2c3ad0e564196805c0e815d16
Esteban Jean <esteban.jean22@sfr.fr>
2026-03-03 14:32:06 +0100

    Frame invincibility + blink character damage

--- Knight-Island/Assets/Scripts/Characters/PlayerHealth.cs
+++ Knight-Island/Assets/Scripts/Characters/PlayerHealth.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Characters
 {
     public class PlayerHealth : MonoBehaviour
     {
+        [SerializeField] private float invincibilityDuration = 1.0f;
         [SerializeField] private float maxHealth = 100f;
+        
         public float CurrentHealth => _currentHealth;
         private float _currentHealth;
+        private bool _isInvincible = false;
+        private SpriteRenderer _spriteRenderer;
         
         public event Action<float, float>  OnHealthChanged;
         public event Action OnDamageTaken;
@@ -18,14 +23,23 @@ namespace Characters
             OnHealthChanged?.Invoke(_currentHealth, maxHealth);
         }
 
+        private void Awake()
+        {
+            _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
         public void TakeDamage(float damage)
         {
+            if (_isInvincible) return;
+            
             _currentHealth -= damage;
             
             _currentHealth = Mathf.Clamp(_currentHealth, 0, maxHealth);
             OnHealthChanged?.Invoke(_currentHealth, maxHealth);
             
             OnDamageTaken?.Invoke();
+
+            StartCoroutine(InvincibilityRoutine());
         }
 
         public void Heal(float amount)
@@ -34,6 +48,25 @@ namespace Characters
             
             _currentHealth = Mathf.Clamp(_currentHealth, 0, maxHealth);
             OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+        }
+
+        private IEnumerator InvincibilityRoutine()
+        {
+            _isInvincible = true;
+            print("[HEALTH] Le personnage est invincible !");
+            
+            float timer = 0f;
+            while (timer < invincibilityDuration)
+            {
+                _spriteRenderer.enabled = !_spriteRenderer.enabled;
+        
+                yield return new WaitForSeconds(0.1f);
+                timer += 0.1f;
+            }
+
+            _spriteRenderer.enabled = true;
+            _isInvincible = false;
+            print("[HEALTH] Invincibilité terminée.");
         }
     }
 }
