8bbe2803272032b648e9f394f3cf15537f6b4498
leo_bg_cf <leobgely@gmail.com>
2026-05-05 15:57:19 +0200

    add attack controller for enemies

---
+++ Knight-Island/Assets/Scripts/Characters/Attack/EnemyAttackController.cs
@@ -0,0 +1,29 @@
+﻿using Characters.Movements;
+using UnityEngine;
+
+namespace Characters.Attack
+{
+    public class EnemyAttackController : CharacterAttackController
+    {
+        [SerializeField] private EnemyMovementController movementController;
+        [SerializeField] private float attackDistance;
+        [SerializeField] private Vector2 attackDistanceOffset;
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position + (Vector3)attackDistanceOffset, attackDistance);
+        }
+
+        private void Update()
+        {
+            if (!movementController.Target) return;
+            if (!IsAttacking &&
+                Vector2.Distance(movementController.Target.position, 
+                    attackDistanceOffset + (Vector2)transform.position) <= attackDistance)
+            {
+                Attack();
+            }
+        }
+    }
+}
